b9dd63453f2f94d8760037de889bb5f940eafe46
Duy Nguyen <duyng@microsoft.coml>
2016-10-30 16:40:16 -0700

    add storage

--- NalFind/MainViewController.swift
+++ NalFind/MainViewController.swift
@@ -4,6 +4,11 @@ import Firebase
 @objc(MainViewController)
 class MainViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    var ref: FIRDatabaseReference!
+    fileprivate var _refHandle: FIRDatabaseHandle!
+    var storageRef: FIRStorageReference!
+    var remoteConfig: FIRRemoteConfig!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -14,6 +19,20 @@ class MainViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         // Dispose of any resources that can be recreated.
     }
 
+    func configureStorage() {
+        storageRef = FIRStorage.storage().reference(forURL: "gs://nalnoxone2016.appspot.com")
+    }
+
+    func sendMessage(withData data: [String: String]) {
+        var mdata = data
+        mdata[Constants.MessageFields.name] = AppState.sharedInstance.displayName
+        if let photoURL = AppState.sharedInstance.photoURL {
+            mdata[Constants.MessageFields.photoURL] = photoURL.absoluteString
+        }
+        // Push data to Firebase Database
+        self.ref.child("messages").childByAutoId().setValue(mdata)
+    }
+    
 //    @IBAction func signOut(_ sender: UIButton) {
 //        let firebaseAuth = FIRAuth.auth()
 //        do {
--- Podfile
+++ Podfile
@@ -9,5 +9,6 @@ target 'NalFind' do
   pod 'Firebase/Core'
   pod 'Firebase/Auth'
   pod 'Firebase/Database'
+  pod 'Firebase/Storage'
 
 end
